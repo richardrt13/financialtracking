365dc7d1fbac19eae6d0e75ee4b38a704ddb7dab
Richard Tomé <richardrt13@gmail.com>
2025-02-14 22:05:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -740,7 +740,7 @@ def main():
                 
                 if st.button("Adicionar Transação"):
                     current_month_index = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 
-                                       'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'].index(month)
+                                       'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'].index(new_month)
                     current_year = year
                     
                     for i in range(repeat_months):
